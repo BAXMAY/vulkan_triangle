20e861d9162a7d60e87528767ed743423bfec449
mee <baramee1407@gmail.com>
2022-06-29 23:47:51 +0700

    Initial Commit

---
+++ triangle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  triangle
+//
+//  Created by Baramee Thunyapoo on 29/6/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
